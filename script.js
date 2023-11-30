f99b0fbb03c79a8fbf4f35de902d2ea2e42a8dc4
Kevin-Heng <s10258007@connect.np.edu.sg>
2023-12-01 01:46:57 +0800

    why js not working

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-<script>
-    
-    function goBack() {
-      window.history.back()
+
+  // script.js
+
+  // Initialize quantity variable
+  let quantity = 0;
+
+  // Function to increase quantity
+  function increaseQuantity() {
+    quantity++;
+    updateQuantity();
+  }
+
+  // Function to decrease quantity
+  function decreaseQuantity() {
+    if (quantity > 0) {
+      quantity--;
+      updateQuantity();
     }
-</script>
+  }
+
+  // Function to update the displayed quantity
+  function updateQuantity() {
+    document.getElementById('quantity').textContent = quantity;
+  }
+
+
+
